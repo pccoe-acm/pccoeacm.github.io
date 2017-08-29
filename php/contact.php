8f2234f2555a111825a0aff429c52a5cc0cf8f11
Atharva Unde <atharvaunde59@users.noreply.github.com>
2017-08-29 23:26:43 +0530

    Contact.php enabled
    Contact.php enabled

--- php/contact.php
+++ php/contact.php
@@ -43,8 +43,8 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
-	$to = 'info@example.com';  // please change this email id
-	$subject = 'Contact Form : Titan - The best downloaded template ever';
+	$to = 'atharv.unde59@gmail.com';  // please change this email id
+	$subject = 'PCCOE ACM : THE STUDENT CHAPTER WEBSITE:';
 
 	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
